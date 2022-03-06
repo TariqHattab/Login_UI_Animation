080eed1332ddda7206596dd02a7b8161fc7d5c1d
TariqHattab <67665495+TariqHattab@users.noreply.github.com>
2022-03-06 11:59:44 +0300

    stracture orgnize

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
-import 'package:custom_animations_playground/auth/first_animation_page.dart';
+import 'package:custom_animations_playground/auth/auth_page.dart';
 import 'package:custom_animations_playground/lang/lang.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
   runApp(const MyApp());
 }
 
@@ -25,7 +30,7 @@ class MyApp extends StatelessWidget {
           supportedLocales: AppLocalizations.supportedLocales,
           localizationsDelegates: AppLocalizations.localizationsDelegates,
           debugShowCheckedModeBanner: false,
-          home: FirstPage(),
+          home: const AuthPage(),
         );
       }),
     );
